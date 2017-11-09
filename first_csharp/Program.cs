fc5bf738f00286c937fb95f6ac0146f81d4f4299
Scott Clampet <scottclampet@Scotts-MacBook-Pro.local>
2017-11-09 11:06:13 -0700

    complete fundamentals1

--- first_csharp/Program.cs
+++ first_csharp/Program.cs
@@ -6,8 +6,34 @@ namespace first_csharp
     {
         static void Main(string[] args)
         {
-            for(i = 1; i <=255; i++) {
-                System.Console.WriteLine(i);
+            // for (int i = 1; i <= 255; i++)
+            // {
+            //     Console.WriteLine(i);
+            // }
+
+            // for(int i = 1; i <= 100; i++)
+            // {
+            //     if(i % 3 == 0) {
+            //         System.Console.WriteLine("Fizz");
+            //     }
+            //     if (i % 5 == 0)
+            //     {
+            //         System.Console.WriteLine("Buzz");
+            //     }
+            // }
+
+            Random rand = new Random();
+            for (int i = 1; i <= 10; i++)
+            {
+                int newRand = rand.Next(1, 100);
+                if(newRand % 3 == 0)
+                {
+                    System.Console.WriteLine("Fizz");
+                }
+                if (newRand % 5 == 0)
+                {
+                    System.Console.WriteLine("Buzz");
+                }
             }
         }
     }
